0c959404dc3cb9a0951689424df9b4412fcd1bf3
robertalanis <rxalanis@gmail.com>
2020-05-25 01:53:17 -0500

    scraper route works

--- server.js
+++ server.js
@@ -35,3 +35,59 @@ mongoose.connect(MONGODB_URI,
     );
 
 // Routes
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get("https://www.bhg.com/gardening/houseplants/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $(".category-page-item ").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children(".category-page-item-content-wrapper")
+          .children("a")
+          .text()
+          .trim(); 
+        result.link = $(this)
+          .children(".category-page-item-content-wrapper")
+          .children("a")
+          .attr("href");
+        result.photo = $(this)
+          .children(".category-page-item-image")
+          .children("a")
+          .children("div")
+          .attr("data-src");
+  
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
+    });
+  });
+
+  app.get("/delete", function(req, res) {
+    db.Article.remove({}, function(err) { 
+        console.log('collection removed') 
+     });
+})  
+
+  // Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
